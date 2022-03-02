52d7708cd4ceb4d22d1345630eb86adb65f54d55
seraaks <aksos843@newschool.edu>
2022-03-01 19:30:25 -0500

    commit

--- trial/function.js
+++ trial/function.js
@@ -13,3 +13,21 @@ document.body.addEventListener("mousemove", evt => {
       stagger: -0.1 });
   
   });
+  function quarter() {
+    window.resizeTo(
+      window.screen.availWidth / 2,
+      window.screen.availHeight / 2
+    );
+  }
+  console.log("Hello, world!");
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.body.style.backgroundColor = "yellow";
+  } else {
+   document.body.style.backgroundColor = "pink";
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
