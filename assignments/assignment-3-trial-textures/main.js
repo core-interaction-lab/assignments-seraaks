c37f7b1dec123664a824b2d87a95daf758478462
seraaks <aksos843@newschool.edu>
2022-04-18 23:15:38 -0400

    commit

--- assignments/assignment-3-trial-textures/main.js
+++ assignments/assignment-3-trial-textures/main.js
@@ -6,80 +6,31 @@ const db = {
 
 const airtableUrl = `https://api.airtable.com/v0/${db.id}/${db.table}?maxRecords=100&view=Grid%20view&api_key=${db.apiKey}`
 
-const fetchTextures = async () => {
-    const response = await fetch(airtableUrl).then(data => data.json());
+const fetchMovies = async () => {
+    const response = await fetch('https://api.airtable.com/v0/${db.id}/${db.table}?maxRecords=100&view=Grid%20view&api_key=${db.apiKey}').then(data => data.json());
+
     console.log(response);
-    const myObject = {
-        title: 'texture',
-    }
-    
-    const myArray= ['texture',334824,'example']
-    
-    console.log (myObject.title)
-    console.log(myArray[0]);
 
-    const container = document.getElementById('textures-container');
-    
-    
-    response.records.forEach((texture)=>{
-        console.log(texture);
-        if (texture.fields.image){
-            console.log(texture.fields.image[0].url);
-            const imageImg = document.createElement('img');
-            imageImg.setAttribute('src',texture.fields.image[0].url);
-            container.append(imageImg);
+    const moviesContainer = document.getElementById('textures-container');
 
-        }
-        
-        
-        
-        if(texture.fields.description){
-            console.log(texture.fields.description);
-            const descriptionEl = document.createElement('p');
-            descriptionEl.innerHTML = texture.fields.description;
-            descriptionEl.classList.add('texture-description');
-            container.append(descriptionEl);
-    
-        }
+    response.records.forEach(texture => {
+        console.log(texture.fields);
+        const imageEl = document.createElement('article');
+        const descriptionEl = document.createElement('div');
+        const maincolorEl = document.createElement('div');
+        const mainmaterialEl = document.createElement('a');
 
-        if(texture.fields.maincolor){
-            console.log(texture.fields.maincolor);
-            const maincolorEl = document.createElement('p');
-            maincolorEl.innerHTML = texture.fields.maincolor;
-            maincolorEl.classList.add('texture-maincolor');
-            container.append(maincolorEl);
-        }
+        descriptionEl.innerHTML = texture.fields.description;
+        maincolorEl.innerHTML = texture.fields.maincolor;
 
-        if(texture.fields.mainmaterial){
-            console.log(texture.fields.mainmaterial);
-            const mainmaterialEl = document.createElement('p');
-            mainmaterialEl.innerHTML = texture.fields.mainmaterial;
-            mainmaterialEl.classList.add('texture-mainmaterial');
-            container.append(mainmaterialEl);
+       
 
-        }
-        if(texture.fields.date){
-            console.log(texture.fields.date);
-            const dateEl= document.createElement('p');
-            dateEl.innerHTML=texture.fields.date;
-            dateEl.classList.add('texture-date');
-            container.append(dateEl);
+        articleEl.append(imageEl, descriptionEl, maincolorEl, mainmaterialEl);
 
-        }
-
-        
-
-        
-        
-
-
-
+        texturesContainer.appendChild(articleEl);
     });
+
+   
 };
 
-
-fetchTextures();
-
-
-
-
+fetchMovies();
