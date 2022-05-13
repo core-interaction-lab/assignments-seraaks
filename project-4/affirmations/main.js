9e57825c5094db8fcf78d1d0d111659145210ef5
seraaks <aksos843@newschool.edu>
2022-05-12 20:18:52 -0400

    commit

---
+++ project-4/affirmations/main.js
@@ -0,0 +1,11 @@
+var madLibs = function() {
+    var storyDiv = document.getElementById("story");
+    var name = document.getElementById("name").value;
+    var adjective = document.getElementById("adjective").value;
+    var noun = document.getElementById("noun").value;
+    var exclamation = document.getElementById("exclamation").value;
+    var verb = document.getElementById("verb").value;
+    
+    storyDiv.innerHTML = name + " ," + verb + " ," + adjective + " ," + noun + ", " + exclamation + "!";
+}
+
--- project-4/meditationmain/main.js
+++ project-4/meditationmain/main.js
@@ -1,3 +1,20 @@
+let popup = document.querySelector(".popup"),
+    button = document.querySelector(".button");
+
+button.addEventListener("click", openPop);
+
+function openPop(){
+  popup.style.display = "Block";
+}
+
+window.addEventListener("click", closePop);
+
+function closePop(e){
+  if(e.target == popup) {
+  popup.style.display = "none";
+}
+}
+
 
 
 const FULL_DASH_ARRAY = 283;
